3d74e95ba12b13639c83b062d4ad90a857ae7f49
ropnya <royalrop@gmail.com>
2023-04-29 20:48:37 +0900

    2023/04/29

--- list.php
+++ list.php
@@ -31,6 +31,23 @@
                 mb_internal_encoding("utf8");
                 $pdo=new PDO("mysql:dbname=lesson01;host=localhost;","root","");
                 $stmt=$pdo->query("select* from acount");
+
+                echo"<table>";
+
+                echo"<tr><th>ID</th><th>名前(姓)</th><th>名前(名)</th><th>カナ(姓)</th><th>カナ(名)</th><th>メールアドレス</th><th>性別</th><th>アカウント権限</th><th>削除フラグ</th><th>登録日時</th><th>更新日時</th><th>操作</th></tr>";
+
+
+                while($row=$stmt->fetch()){
+                    echo"<tr>";
+                    echo"<td>{$row['id']}</td>";
+                    echo"<td>{$row['family_name']}</td>";
+                    echo"<td>{$row['last_name']}</td>";
+                    echo"<td>{$row['family_name_kana']}</td>";
+                    echo"<td>{$row['last_name_kana']}</td>";
+                    echo"<td>{$row['mail']}</td>";
+                }
+
+                echo"</table>";
                 
             ?>
         </main>
